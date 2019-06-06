ac125cd5abc9e7663b0e19b7cb1aa6914d8bad9c
Jose <jpm_0593@hotmail.com>
2019-06-06 11:33:40 -0400

    Javascript

---
+++ javascript_101/01_ejercicio.js
@@ -0,0 +1,17 @@
+//My personal information
+
+
+var anos= 25;
+console.log
+var dia= 5;
+var mes= "diciembre";
+var ano_de_nacimiento=1993;
+
+document.write(dia + "  " + mes + " " + ano_de_nacimiento + "  ");
+
+//console.log
+
+var nombre = "JosePerez";
+console.log(nombre);
+
+document.write(maria)
---
+++ javascript_101/jay.js
@@ -0,0 +1,97 @@
+//(Usar comentarios)
+
+
+
+/*
+1. Hacer un script que pida al
+usuario digite su nombre, edad y si esta casado
+Imprimir esos valores en la pagina y por la consola.
+
+2. Hacer un script que tenga 3 variables
+    (Usar comentarios multilinea)
+pais_nombre;
+pais_capital;
+pais_habitantes;
+pais_tiene_bandera;
+Imprimir los datos por la consola.
+
+3. Hacer un script con tres variables
+    (Usar comentarios de 1 sola linea)
+    (Esta informacion es suya)
+estudiante_nombre;
+estudiante_edad;
+estudiante_tiene_carro;
+Usar los tipos de dato string, number y boolean;
+Imprimir los valores por la pagina en un h1;  */
+
+
+//Usar comentario---------------------------------------------------------------------------
+
+
+//var nombre = prompt("¿cual es su nombre?");
+//var edad = prompt("¿cual es su edad?");
+//var estado_civil = prompt("¿estas casado?");
+
+//console.log(nombre);
+//console.log(edad);
+//console.log(estado_civil);
+
+//document.write(nombre);
+//document.write(edad);
+//document.write(estado_civil);
+
+
+
+//Usar comentario Multinea----------------------------------------------------------------------
+
+
+  //var pais_nombre =("Republica Dominicana");
+  //var pais_capital =("Santo Domingo");
+  //var pais_habitantes =("10,000,000");
+  //var pais_tiene_bandera =("Si");
+
+//console.log(pais_nombre);
+//console.log(pais_capital);
+//console.log(pais_habitantes);
+//console.log(pais_tiene_bandera);
+
+
+// Hacer un script con tres variables-----------------------------------------------------------
+
+//var estudiante_nombre=("Jose");
+//var estudiante_edad = 25;
+//var estudiante_tiene_carro= ("true");
+
+//document.write(`<h1>${estudiante_nombre}</h1>`);
+//document.write(`<h1>${estudiante_edad}</h1>`);
+//document.write(`<h1>${estudiante_tiene_carro}</h1>`);
+
+
+//hacer un usuario----------------------------------------------------------------------------------
+
+//var nombre = prompt("¿Cual es tu nombre?");
+//var edad = prompt("Cual es tu edad?");
+
+//console.log(nombre);
+//console.log(edad);
+
+//document.write("nombre");
+//document.write(edad);
+
+
+//Nombre,Apellidos y ciudad------------------------------------------------------------------------------
+
+var nombre = "Jose";
+var apellido = "Perez"
+var ciudad = "Santiago";
+
+var nombre = prompt ("¿Cual es tu nombre?");
+var apellido = prompt ("¿Cual es tu apellido?");
+var ciudad = prompt ("¿Cual es tu ciudad?");
+
+document.write(`<h1>Bienvenido ${nombre} ${apellido} a facebook.</h1>`);
+document.write(`<h2>Actualmente vives en ${ciudad}.</h2>`);
+document.write(`<h3>Gracias por registrarte ${nombre} en nuestro sitio.</h3>`);
+
+
+
